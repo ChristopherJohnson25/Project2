9c6f869376c29b63d0c5903d251ae29c29d85684
Chris Johnson <johnsonchris98@gmail.com>
2015-12-02 09:35:30 -0500

    Update RB

--- database/schema.sql
+++ database/schema.sql
@@ -4,15 +4,15 @@ CREATE TABLE contents (
 	id INTEGER PRIMARY KEY,
 	post TEXT,
 	img_url TEXT,
-	post_date DATE,
-	edit_date DATE
+	post_date DATETIME,
+	edit_date DATETIME
 );
 
 CREATE TABLE users (
 	id INTEGER PRIMARY KEY,
 	user_name VARCHAR,
 	avatar_url TEXT,
-	join_date DATE
+	join_date DATETIME
 );
 
 CREATE TABLE contents_users (
